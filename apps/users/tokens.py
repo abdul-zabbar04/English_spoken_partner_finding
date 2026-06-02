c2856d7415d5fea8f20da40a89ce1633a70e5c63
Abdul Zabbar <a4zabbar@gmail.com>
2026-06-03 04:14:03 +0600

    feat: add secure email verification token

---
+++ apps/users/tokens.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+
+class EmailVerificationTokenGenerator(
+    PasswordResetTokenGenerator
+):
+
+    def _make_hash_value(self, user, timestamp):
+        return (
+            f"{user.pk}"
+            f"{timestamp}"
+            f"{user.is_verified}"
+        )
+
+
+email_verification_token = (
+    EmailVerificationTokenGenerator()
+)
