6b732107eb42ad8621053595d6c8cd488c33b90c
Abdul Zabbar <a4zabbar@gmail.com>
2026-06-03 03:56:11 +0600

    feat: optimize custom user admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    # it's overriding the default add_fieldsets to remove the username field
+    add_fieldsets = None
+    filter_horizontal = ()
+
+    list_display = ("email", "is_verified", "is_staff", "is_active")
+    list_filter = ("is_verified", "is_staff", "is_active")
+
+    ordering = ("email",)
+    search_fields = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_verified",
+                    "is_staff",
+                    "is_superuser",
+                    "is_active",
+                )
+            },
+        ),
+        ("Dates", {"fields": ("last_login", "date_joined")}),
+    )
