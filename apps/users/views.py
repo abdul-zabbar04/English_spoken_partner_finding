7d1b4adc3aea1e39342609e316937c93eaedfca2
Abdul Zabbar <a4zabbar@gmail.com>
2026-06-08 20:46:06 +0600

    feat: added Register views

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(
+            data=request.data
+        )
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(
+            {
+                "message": "User registered successfully. Please verify your email."
+            },
+            status=status.HTTP_201_CREATED,
+        )
