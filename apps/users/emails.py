83c4c4b6288da1b866c84c79ed03a9216865ad15
Abdul Zabbar <a4zabbar@gmail.com>
2026-06-12 10:14:28 +0600

    send email verification improvement

--- apps/users/emails.py
+++ apps/users/emails.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-def send_verification_email(user: User) -> None:
+def send_verification_email(*, user: User) -> None:
     """
     Send account verification email.
     """
