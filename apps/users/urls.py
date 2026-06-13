3ae67aa024e43def7a471ecd2cf6d6fa8a0ae0c6
Abdul Zabbar <a4zabbar@gmail.com>
2026-06-14 00:19:08 +0600

    refresh, me and logout urls added

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
     RegisterAPIView,
     VerifyEmailAPIView,
     LoginAPIView,
+    MeAPIView,
+    LogoutAPIView,
 )
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()),
     path("verify-email/", VerifyEmailAPIView.as_view(), name="verify-email"),
     path("login/", LoginAPIView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view()),
+    path("me/", MeAPIView.as_view()),
+    path("logout/", LogoutAPIView.as_view()),
 ]
