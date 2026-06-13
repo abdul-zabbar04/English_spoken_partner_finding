1f07718d9bf37e3f74fe7878d9c104fa0de498f8
Abdul Zabbar <a4zabbar@gmail.com>
2026-06-13 22:09:13 +0600

    refactor: rotate_refresh_tokens set False initially

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,7 +75,7 @@ SIMPLE_JWT= {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days= 7),
     'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'BLACKLIST_AFTER_ROTATION': False,
     'AUTH_HEADER_TYPES': ('Bearer',),
     'UPDATE_LAST_LOGIN': True,
 }
