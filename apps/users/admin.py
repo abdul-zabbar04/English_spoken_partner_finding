1fcc4fbac7be4a125185ab6fa9e58e98c1dbe145
Abdul Zabbar <a4zabbar@gmail.com>
2026-06-14 00:32:15 +0600

    outstandingToken deleting option added in admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -32,3 +32,16 @@ class CustomUserAdmin(UserAdmin):
         ),
         ("Dates", {"fields": ("last_login", "date_joined")}),
     )
+
+# Register the OutstandingToken model to manage JWT tokens in the admin interface
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
+
+
+# 1. Remove default admin registration
+admin.site.unregister(OutstandingToken)
+
+@admin.register(OutstandingToken)
+class OutstandingTokenAdmin(admin.ModelAdmin):
+    list_display = ("user", "jti", "created_at", "expires_at")
+    actions = ["delete_selected"]
+
