9fbe29152e2bdb78684fde2f7ffeda41e35a2857
Abdul Zabbar <a4zabbar@gmail.com>
2026-07-04 19:52:12 +0600

    chore(config): configure cors-headers for cross-origin access

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
